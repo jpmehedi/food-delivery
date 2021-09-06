8f4438d6e459280ee45e6a5da92f284be26479a2
Mehedi Hasan <mehedi@augnitive.com>
2021-09-06 13:35:06 +0600

    food delivery app Skeliton

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class AppColor{
+  static const Color primaryColor = Color(0xffFFA500);
+  static const Color secondaryColor = Color(0xffFFA500);
+  static const Color primaryTextColor = Color(0xffFFA500);
+  static const Color secondaryTextColor = Color(0xffFFA500);
+}
---
+++ lib/views/signup/signup.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class SignUpScreen extends StatelessWidget {
+  static const String PATH = "/SignUpScreen";
+  const SignUpScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
